467aec9386ede5cffd7a395820684877b7c68e01
RPCodes121 <rudraprakashdas977@gmail.com>
2026-07-23 07:27:18 +0530

    implement file reading

--- FileDetector.c
+++ FileDetector.c
@@ -13,8 +13,10 @@ void check_files(Drive drives[], int drive_count)
         ULARGE_INTEGER total_space = drives[i].total_space;
         ULARGE_INTEGER occupied_space = drives[i].occupied_space;
         unsigned long long usage=(occupied_space.QuadPart*100ULL)/total_space.QuadPart;
+        int file_count=0;
+        
         if(usage>STORAGE_THRESHOLD){
-            printf("drive %s is almost full\n",drives[i].name);
+            
             
         }
         else{
